f76034a5b8d932f1b32d407619aaa3e6e867d92d
LWeihs <lennart.weihs@web.de>
2020-03-07 15:53:12 +0100

    updated python script
    - handle multiple options correctly
    - split strings in diagrams based on pyphen dictionary hyphenation

--- survey_evaluation.py
+++ survey_evaluation.py
@@ -9,6 +9,7 @@ import sys
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.backends.backend_pdf import PdfPages
+import pyphen
 
 #set up script params
 desc = ('Get evaluation data from your mail server and writes results to a '
@@ -32,7 +33,8 @@ parser.add_argument('-nopie', action='store_true',
                     help='Disable plotting of pie plots')
 args = parser.parse_args()
 
-#set variables used in script
+#set variables used in script, apply changes to colors, text lengths, etc. here
+german_dict = pyphen.Pyphen(lang='de_DE')
 login = args.login
 password = args.password
 pop_server = (args.pop_server if args.pop_server else 'pop3.web.de')
@@ -47,6 +49,8 @@ colors_ten = ['red', 'darkorange', 'gold', 'yellowgreen', 'limegreen',
 txt_file_name = 'results.txt'
 bar_file_name = 'result_bars.pdf'
 pie_file_name = 'result_pies.pdf'
+pie_wrap_len = 20
+bar_wrap_len = 10
 
 #establish server connection
 mail_box = None
@@ -59,12 +63,13 @@ except Exception as err:
     exit(1)
 print('Connected to mail server...')
 
-#gather results in dict
+#prepare to gather results in dict
 results = {}
 def make_entry(key, val):
     if not key in results:
         results[key] = []
-    results[key].append(val)
+    for entry in val:
+        results[key].append(entry)
 
 #fetch data from mails and write progress to stdout
 num_messages = len(mail_box.list()[1])
@@ -90,17 +95,32 @@ for i in range(num_messages):
         questions = []
         answers = []
         soup = BeautifulSoup(content, 'html.parser')
-        wrap = soup.find(class_='mcnTextContent')
-        tds = wrap.find_all('td')
+        wrapper = soup.find(class_='mcnTextContent')
+        tds = wrapper.find_all('td')
     except:
         continue
+    #get individual questions and answers based on exact td content
     for answer in tds:
-        answer = str(answer.contents[0])
-        m = re.search('<strong>(.*)<\/strong>', answer)
-        if m:
-            questions.append(m.group(1))
-        else:
-            answers.append(answer)
+        q_answers = []
+        for part in answer.contents:
+            part = str(part)
+            if part == '<br/>':
+                continue
+            #check if m is question
+            m = re.search('^<strong>(.*)<\/strong>$', part)
+            if m:
+                questions.append(m.group(1))
+                continue
+            #check if pure number answer
+            m = re.search('^vergebene Punkte: (.*) \(0 min \/ 10 max\)$', part)
+            if m:
+                q_answers.append(m.group(1))
+                continue
+            #default: regular text answer
+            q_answers.append(part.strip())
+        if (len(q_answers)):
+            answers.append(q_answers)
+    #sanity check, then make result entry
     q_len = len(questions)
     if (q_len != len(answers)):
         continue
@@ -109,7 +129,7 @@ for i in range(num_messages):
 sys.stdout.write('All done.\n')
 sys.stdout.flush()
 
-#transform results to wanted output format
+#transform results to wanted (sorted by occurence) output format
 sorted_results = {}
 for question, answers in results.items():
     counts = {}
@@ -130,7 +150,7 @@ def write_qa(res_file, question, sorted_counts):
         res_file.write('\t{} (x{})\n'.format(answer, times))
     res_file.write('\n')
     
-#prepare plotting
+#prepare fetching of correct color
 def get_colors(nr_answers):
     if nr_answers <= 2:
         return colors_simple
@@ -138,8 +158,53 @@ def get_colors(nr_answers):
         return colors_five
     else:
         return colors_ten
+
+#prepare wrapping of words with hyphenation - fails on very short line_len
+def wrap_word(full_str, line_len):
+    words = full_str.split()
+    lines = []
+    cur_line = ''
+    #for adding word parts at beginning of words
+    def extend_nw(part):
+        nonlocal cur_line
+        if (len(cur_line) + len(part) > line_len):
+            lines.append(cur_line)
+            cur_line = part
+        else:
+            cur_line += part
+    #for adding word parts in middle of word
+    def extend(part):
+        nonlocal cur_line
+        #+1 to account for hyphen
+        if len(cur_line) + len(part) +1 > line_len:
+            cur_line += '-'
+            lines.append(cur_line)
+            cur_line = part
+        else:
+            cur_line += part
+    #wrap creation
+    for word in words:
+        positions = german_dict.positions(word)
+        if (len(positions) == 0):
+            extend_nw(word)
+            cur_line += ' '
+            continue
+        cursor_1 = 0
+        cursor_2 = positions[0]
+        extend_nw(word[cursor_1:cursor_2])
+        for i in range(1, len(positions)):
+            cursor_1 = cursor_2
+            cursor_2 = positions[i]
+            extend(word[cursor_1:cursor_2])
+        extend(word[cursor_2:len(word)])
+        cur_line += ' '
+    lines.append(cur_line)
+    lines = [l.rstrip() for l in lines]
+    return '\n'.join(lines)
     
+#prepare different plot functions
 def plot_bars(question, ratings, labels):
+    labels = [wrap_word(l, bar_wrap_len) for l in labels]
     nr_answers = len(labels)
     x = [i for i in range(nr_answers)]
     fig, axis = plt.subplots()
@@ -148,13 +213,12 @@ def plot_bars(question, ratings, labels):
     plt.xticks(x, labels)
     plt.title(question)
     plt.tight_layout()
-    pass
     
 def plot_pie(question, ratings, labels):
+    labels = [wrap_word(l, pie_wrap_len) for l in labels]
     plt.pie(ratings, labels=labels, colors=get_colors(len(labels)))
     plt.title(question)
     plt.tight_layout()
-    pass
 
 def start_write_plot_to_pdf():
     plt.figure()
